327a72ac5430a762b46012572d7e749e5eaf49c1
KeeratiNon <keeratinon3@gmail.com>
2024-07-03 12:12:59 +0700

    Do ex1-3

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+function printStar(n) {
+    let finalResult = ""
+    for (let i = 1; i<=n; i++) {
+    let result = ""
+    let row = "*"
+    for (let j = 1; j<=i; j++) {
+        result += row + "\t"
+    }
+    finalResult += result + "\n"
+   }
+   return finalResult
 }
+
+console.log(printStar(5))
